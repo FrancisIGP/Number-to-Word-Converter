ec1fe9f1bddd3380aa33d085ff707596036efbb0
Francis <75497349+FrancisIGP@users.noreply.github.com>
2021-11-01 20:45:07 +0800

    Add files via upload

---
+++ NumberConverter/wordList.java
@@ -0,0 +1,46 @@
+package NumberConverter;
+
+public class wordList
+{
+        public String[][] wordFormat = 
+        {
+
+                        { " ", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine" },
+
+                        { " ", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen",
+
+                        "Eighteen", "Nineteen" },
+
+                        { " ", " ", "Twenty", "Thirty", "Fourty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" } 
+        };
+
+        public String[] commas = 
+        {
+
+                        "Hundred", "Thousand", "Million", "Billion", "Trillion", "Quadrillion", "Quintillion",
+
+                        "Sextillion", "Septillion", "Octillion", "Nonillion", "Decillion", "Undecillion",
+
+                        "Duodecillion", "Tredecillion", "Quattuordecillion", "Quindecillion", "Sedecillion",
+
+                        "Septendecillion", "Octodecillion", "Novendecillion", "Vigintillion", "Unvigintillion",
+
+                        "Duovigintillion", "Tresvigintillion", "Quattourvigintillion", "Quinvigintillion",
+
+                        "Sesvigintillion", "Septenvigintillion", "Octovigintillion", "Novemvigintillion",
+
+                        "Trigintillion", "Untrigintillion", "Duotrigintillion", "Trestrigintillion", "Quattuortrigintillion",
+
+                        "Quintrigintillion", "Sestrigintillion", "Septentrigintillion", "Octotrigintillion", "Noventrigintillion", 
+                        
+                        "Quadragintillion", "Quinquagintillion", "Sexagintillion", "Septuagintillion", "Octogintillion", "Nonagintillion",
+                
+                        "Centillion", "Uncentillion", "Decicentillion", "Undecicentillion", "Viginticentillion", "Unviginticentillion", 
+                        
+                        "Trigintacentillion", "Quadragintacentillion", "Quinquagintacentillion", "Sexagintacentillion", "Septuagintacentillion"
+                        
+                        , "Octogintacentillion", "Nonagintacentillion", "Ducentillion", "Trecentillion", "Quadringentillion", "Quingentillion", 
+                        
+                        "Sescentillion", "Septingentillion", "Octingentillion", "Nongentillion", "Millinillion" 
+        };
+}
